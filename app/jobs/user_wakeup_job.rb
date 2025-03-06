802b1ce017278c4129c6689098b3c5a5d938f3fe
Hendra Gunawan <hendragunz@codecampz.com>
2025-03-06 09:51:02 +0700

    check to make sure is user was sleep

--- app/jobs/user_wakeup_job.rb
+++ app/jobs/user_wakeup_job.rb
@@ -2,6 +2,7 @@ class UserWakeupJob < ApplicationJob
   queue_as :default
 
   def perform(user, wakeup_at)
+    return unless user.sleep?
     ActiveRecord::Base.transaction do
       user.sleep_logs.create!(sleep_at: user.sleep_at, wakeup_at: wakeup_at)
       user.update!(sleep_at: nil)
