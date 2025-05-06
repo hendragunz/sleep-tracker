2da33953a8b07c9294577f4999d2c953e975506b
Hendra Gunawan <hendragunz@codecampz.com>
2025-05-06 23:48:27 +0700

    add important indexes for performance / sql query

---
+++ db/migrate/20250506164420_add_index_for_performance.rb
@@ -0,0 +1,9 @@
+class AddIndexForPerformance < ActiveRecord::Migration[8.0]
+  def change
+    add_index :users, :id
+    add_index :users, :authentication_token
+    add_index :sleep_logs, :sleep_at
+    add_index :sleep_logs, :wakeup_at
+    add_index :sleep_logs, :duration
+  end
+end
